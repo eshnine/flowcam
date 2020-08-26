bbe1be5846378c296fcc716a0ecee379515fe090
esh <esh.nine@pm.me>
2020-08-26 13:02:18 +0800

    allow to only show the self camera and not record

--- flowcam.sh
+++ flowcam.sh
@@ -2,7 +2,12 @@
 
 function start_recording(){
 	local file_name=${@-"tmp"}
-  ffplay -window_title esh -f v4l2 -framerate 25 -video_size 320x240 -i /dev/video0 &
+  if [ "$2" = "self" ]; then
+    ffplay -window_title esh -f v4l2 -framerate 25 -video_size 320x240 -i /dev/video0 &
+  fi
+  if [ "$3" = "only" ]; then
+    exit
+  fi
   ffmpeg -y -thread_queue_size 512  -f x11grab -video_size 1280x800 -framerate 30 -i :0.0  -c:v h264_qsv  -thread_queue_size 512 -f alsa -ac 2 -i hw:0  "/tmp/$file_name.mp4"
 }
 
@@ -13,13 +18,13 @@ dialog --title "Flow" --backtitle "FlowCam"  --inputbox "What is your flow?" 8 6
 
 trap "rm $TEMP_OUTPUT_FILE_NAME; exit" SIGHUP SIGINT SIGTERM
 
-user_respose=$?
+user_response=$?
 
 flow_name=$(<$TEMP_OUTPUT_FILE_NAME)
 
-case $user_respose in
+case $user_response in
   0)
-    start_recording $flow_name
+    start_recording $flow_name $1 $2
     ;;
   1)
     echo "Cancel pressed."
